897fe6bea679f2dc9ab1846deeb1cdbd713bf680
JonGithab <80584620+JonGithab@users.noreply.github.com>
2026-01-07 02:56:46 +0000

    update

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,12 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
 
+// Add Inter font from Google Fonts
+const _gf = document.createElement('link')
+_gf.rel = 'stylesheet'
+_gf.href = 'https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap'
+document.head.appendChild(_gf)
+
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
 	<App />
